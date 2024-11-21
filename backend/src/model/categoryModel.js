e4e6c7348ce25bb25bb7a65a888c6d45b74a6450
TuanVux <tuanvukg2703@gmail.com>
2024-11-21 13:37:30 +0700

    still fix

--- backend/src/model/categoryModel.js
+++ backend/src/model/categoryModel.js
@@ -2,9 +2,14 @@ import connection from "../DB/connectDB";
 
 const createCategory = async ({ category_name, description }) => {
     try {
-        const [result] = await connection.execute("")
+        const [result] = await connection.execute(`INSERT INTO categories(category_name, description) VALUES(?, ?)`, [category_name, description])
+        return result
     }
-    catch {
-
+    catch (error) {
+        console.log('lỗi gì đó không biết: ', error);
+        throw error;
     }
+}
+export default {
+    createCategory
 }
--- backend/src/router/webRouter.js
+++ backend/src/router/webRouter.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import getHomePage from '../Controller/HomeController'
-
+import { authorizeAdmin, authenticate } from '../midderwere/midderwere'
+import CategoriesController from '../Controller/CategoriesController'
 
 const router = express.Router()
 const initWebRoute = (app) => {
     router.get('/', getHomePage)
 
 
-
-
+    //category
+    // router.post('/user/create', AuthMiddleware.checkRole("0"), UserController.createUser)
+    router.get('/staff/category', authenticate, authorizeAdmin, (req, res) => {
+        res.send('Welcome Admin');
+    });
     return app.use('/', router)
 }
 export default initWebRoute
