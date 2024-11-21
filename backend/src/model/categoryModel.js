b3e6256b8d375da4d3381647e3363ed0c1aee1c0
TuanVux <tuanvukg2703@gmail.com>
2024-11-21 13:36:32 +0700

    fixing

---
+++ backend/src/model/categoryModel.js
@@ -0,0 +1,10 @@
+import connection from "../DB/connectDB";
+
+const createCategory = async ({ category_name, description }) => {
+    try {
+        const [result] = await connection.execute("")
+    }
+    catch {
+
+    }
+}
