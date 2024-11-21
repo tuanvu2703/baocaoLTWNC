977e7bf31005365f5f48142ef67a6238dfd59d4d
TuanVux <tuanvukg2703@gmail.com>
2024-11-21 14:37:00 +0700

    category

--- backend/src/Controller/CategoriesController.js
+++ backend/src/Controller/CategoriesController.js
@@ -1,4 +1,4 @@
-import categoryModel from "../model/categoryModel";
+import * as categoryModel from '../model/categoryModel';
 
 const createCategory = async (req, res) => {
     try {
@@ -6,11 +6,20 @@ const createCategory = async (req, res) => {
         await categoryModel.createCategory(category_name, description)
         res.status(200).json({ message: 'create seccesfully', data: req.body })
     } catch (error) {
-        console.log('lỗi gì đó không biếc:', error);
+        console.error('Error creating category:', error);
+        res.status(500).json({ message: 'Internal server error' });
     }
 }
-
+const getCreateCategoryPage = (req, res) => {
+    return res.render("index",
+        {
+            title: 'Danh mục',
+            page: 'categories',
+        }
+    )
+}
 
 export default {
-    createCategory
+    createCategory,
+    getCreateCategoryPage
 }
