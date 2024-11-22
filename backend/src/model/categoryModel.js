512ae20e2fdfd69761e2809548c049b0c17e71ca
TuanVux <tuanvukg2703@gmail.com>
2024-11-22 14:10:30 +0700

    total product on category

--- backend/src/model/categoryModel.js
+++ backend/src/model/categoryModel.js
@@ -11,7 +11,7 @@ const createCategory = async (category_name, description) => {
     }
 }
 const getAllCategory = async () => {
-    const [data, fields] = await connection.execute("SELECT * FROM `categories`")
+    const [data, fields] = await connection.execute("SELECT c.*, COUNT(p.product_id) AS total_products FROM categories c LEFT JOIN products p ON c.id = p.category_id GROUP BY c.category_name")
     return data
 }
 export default {
