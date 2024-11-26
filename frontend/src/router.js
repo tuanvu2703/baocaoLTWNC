92a1a473c1ed73ea9e10d111f66f1f32466ee261
VyVictory <vyvictory2003@gmail.com>
2024-11-26 17:12:14 +0700

    fix router

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,24 +1,49 @@
 // import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layout";
 import App from "./App";
 import NoPage from "./views/NoPage";
 import ProductPage from "./views/ProductPage";
 import AllProduct from "./components/AllProduct";
 import ProductByCategory from "./components/ProductByCategory";
-export default function Router() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<App />} />
-                    <Route path="/product" element={<ProductPage />}>
-                        <Route index element={<AllProduct />} />
-                        <Route path="/product/category" element={<ProductByCategory />} />
-                    </Route>
-                    <Route path="*" element={<NoPage />} />
-                </Route>
-            </Routes>
-        </BrowserRouter >
-    )
-}
+// export default function Router() {
+//     return (
+//         <BrowserRouter>
+//             <Routes>
+//                 <Route path="/" element={<Layout />}>
+//                     <Route index element={<App />} />
+//                     <Route path="/product" element={<ProductPage />}>
+//                         <Route index element={<AllProduct />} />
+//                         <Route path="/product/category" element={<ProductByCategory />} />
+//                     </Route>
+//                     <Route path="*" element={<NoPage />} />
+//                 </Route>
+//             </Routes>
+//         </BrowserRouter >
+//     )
+// }
+export const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "/", element: <App /> },
+            {
+                path: "product",
+                element: <ProductPage />,
+                children: [
+                    { index: true, element: <AllProduct /> },
+                    {
+                        path: "category", // Dynamic route for categories
+                        element: <ProductByCategory />,
+                    },
+                ]
+            }
+        ]
+    },
+    {
+        path: "/*",
+        element: <NoPage />,
+    },
+]);
